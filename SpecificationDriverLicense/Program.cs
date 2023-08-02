31cd50c0ff2d2d13105b7eec0f3942f25e2ec74e
Carlos Perez Salazar <cps172779@gmail.com>
2023-08-02 17:51:19 -0600

    inject data to test

--- SpecificationDriverLicense/Program.cs
+++ SpecificationDriverLicense/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using SpecificationDriverLicense;
+using System.Data;
+using System.Text.Json;
+
+int edadParaEvaluar = 18 ;
+
+var person = new PersonModel
+{
+    Altura = 6,
+    Id = 1,
+    Edad = 24,
+    Nombre = "Vaxi Drez"
+};
+
+
+var result = EvaluateDriverLicenceByEdad(edadParaEvaluar, person) ? $"{person.Nombre} obtuvo la licencia" : "Licencia negada";
+
+Console.WriteLine(result);
+
+bool EvaluateDriverLicenceByEdad(int edadParaEvaluar, PersonModel personModel)
+{
+    var specification = new DriverLicenseBySpecification(edadParaEvaluar);
+
+    return specification.IsCumpleReglas(personModel);
+}
+
+
+var personData = File.ReadAllText(@"person.json");
+var persons = JsonSerializer.Deserialize<List<PersonModel>>(personData);
+
+var personsWithLicense = EvaluateColletionPeronByLicenseDriver(persons, edadParaEvaluar);
+
+personsWithLicense.ToList().ForEach(person => Console.WriteLine($"{person.Nombre}  {person.Edad} obtuvo la licencia"));
+
+IEnumerable<PersonModel> EvaluateColletionPeronByLicenseDriver (IEnumerable<PersonModel> personsModel, int edadParaEvaluar)
+{
+    return personsModel.Where(x => EvaluateDriverLicenceByEdad(edadParaEvaluar, x));
+}
